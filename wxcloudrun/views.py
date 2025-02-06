0b97477283d12bd4b78d6cee2b13227a11ecb705
Yakun Jin <jinyakun0629@gmail.com>
2025-02-06 16:58:13 +0800

    feat: 修改appid

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -78,7 +78,7 @@ def get_qr():
     """
     try:
         # 获取access_token (微信云托管环境下自动鉴权)
-        app_id = 'wx580dad6261cf35c6'
+        app_id = 'wx622594f6f81fc054'
         token_url = f'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={app_id}'
         token_response = requests.get(token_url)
         access_token = token_response.json().get('access_token')
@@ -128,27 +128,27 @@ def handle_wx_event():
         # 解析XML消息
         xml_data = request.data
         root = ET.fromstring(xml_data)
-        
+
         # 获取消息类型
         msg_type = root.find('MsgType').text
         if msg_type != 'event':
             return 'success'
-            
+
         # 获取事件类型
         event = root.find('Event').text
         if event != 'subscribe':
             return 'success'
-            
+
         # 获取用户openid
         openid = root.find('FromUserName').text
-        
+
         # 获取场景值
         event_key = root.find('EventKey')
         scene_str = 'default'
         if event_key is not None:
             # EventKey格式为：qrscene_xxx，需要去掉前缀
             scene_str = event_key.text.replace('qrscene_', '') if event_key.text else 'default'
-        
+
         # 保存用户信息
         user = Users()
         user.openid = openid
@@ -156,9 +156,9 @@ def handle_wx_event():
         user.stime = datetime.now()
         db.session.add(user)
         db.session.commit()
-        
+
         return 'success'
-        
+
     except Exception as e:
         # 记录错误但返回success，避免微信服务器重试
         print(f'处理微信事件失败: {str(e)}')
